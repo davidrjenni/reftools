c94058db3aff2d4d807a119a1d77b82e12531674
David R. Jenni <david.r.jenni@gmail.com>
2017-08-07 21:48:22 +0200

    cmd/fillstruct: add GoDoc.

--- cmd/fillstruct/main.go
+++ cmd/fillstruct/main.go
@@ -2,6 +2,55 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// Fillstruct fills a struct literal with default values.
+//
+// For example, given the following types,
+//
+//	type User struct {
+//		ID   int64
+//		Name string
+//		Addr *Address
+//	}
+//
+//	type Address struct {
+//		City   string
+//		ZIP    int
+//		LatLng [2]float64
+//	}
+//
+// the following struct literal
+//
+//	var frank = User{}
+//
+// becomes:
+//
+//	var frank = User{
+//		ID:   0,
+//		Name: "",
+//		Addr: &Address{
+//			City: "",
+//			ZIP:  0,
+//			LatLng: [2]float64{
+//				0.0,
+//				0.0,
+//			},
+//		},
+//	}
+//
+// after applying fillstruct.
+//
+// Usage:
+//
+// 	% fillstruct [-modified] -file=<filename> -offset=<byte offset>
+//
+// Flags:
+//
+// -file:     filename
+//
+// -modified: read an archive of modified files from stdin
+//
+// -offset:   byte offset of the struct literal
+//
 package main
 
 import (
